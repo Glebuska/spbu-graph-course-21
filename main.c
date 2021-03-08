9a06f87edc34a86e642cb98e5ed03f2044fe4b2b
Glebuska <gleb.mirzazyanov@gmail.com>
2021-03-08 21:23:50 +0300

    working version

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <iostream>
+//include <iostream>
 #include <GraphBLAS.h>
 #define LAGRAPH_FREE_ALL { GrB_Vector_free (&v) ; GrB_Vector_free (&q) ; }
-using namespace std;
+//using namespace std;
 
 GrB_Info bfs // push-only BFS
         (
@@ -13,14 +13,14 @@ GrB_Info bfs // push-only BFS
         // in the pre-order traversal sequence.
 {
     GrB_Info info ;
-    GrB_Vector q = nullptr ; // nodes visited at each level
-    GrB_Vector v = nullptr ; // result vector
-    if (s == nullptr) return GrB_NO_VALUE;
+    GrB_Vector q = NULL ; // nodes visited at each level
+    GrB_Vector v = NULL ; // result vector
+    if (s == NULL) return GrB_NO_VALUE;
     GrB_Index n, nvals ;
     GrB_Matrix_nrows (&n, A) ;
 // create an empty vector v, and make it dense
     GrB_Vector_new (&v, (n > INT32_MAX) ? GrB_INT64 : GrB_INT32, n) ;
-    GrB_Vector_assign(v, nullptr, nullptr, 0, GrB_ALL, n, nullptr) ;
+    GrB_Vector_assign(v, NULL, NULL, 0, GrB_ALL, n, NULL) ;
 // create a boolean vector q, and set q(source) to true
     GrB_Vector_new (&q, GrB_BOOL, n) ;
     GrB_Vector_setElement_INT32(q, true, source) ;
@@ -28,16 +28,16 @@ GrB_Info bfs // push-only BFS
     for (int64_t level = 1 ; level <= n ; level++)
     {
 // v<q> = level
-        GrB_Vector_assign_BOOL(v, q, nullptr, level, GrB_ALL, n, GrB_DESC_S) ;
+        GrB_Vector_assign_BOOL(v, q, NULL, level, GrB_ALL, n, GrB_DESC_S) ;
 // break if q is empty
         GrB_Vector_nvals (&nvals, q) ;
         if (nvals == 0) break ;
 // q’<!v> = q’*A
-        GrB_vxm (q, v, nullptr, GxB_ANY_PAIR_BOOL, q, A, GrB_DESC_RC) ;
+        GrB_vxm (q, v, NULL, GxB_ANY_PAIR_BOOL, q, A, GrB_DESC_RC) ;
     }
 // free workspace and return result
     (*s) = v ; // return result
-    v = nullptr ; // set to NULL so LAGRAPH_FREE_ALL doesn’t free it
+    v = NULL ; // set to NULL so LAGRAPH_FREE_ALL doesn’t free it
     LAGRAPH_FREE_ALL;
     return (GrB_SUCCESS) ;
 }
